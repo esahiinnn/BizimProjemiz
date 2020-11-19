710fd5c126bfe22f168697efc8d3ccf4d0c1b7cc
esahiinnn <73313111+esahiinnn@users.noreply.github.com>
2020-11-19 18:42:22 +0300

    Update BizimProje.java

--- src/BizimProje.java
+++ src/BizimProje.java
@@ -4,7 +4,7 @@ public class BizimProje {
 	public static void main(String[] args) {
 		
 		System.out.println("18:34 - Bizim Proje");
-		System.out.println("18:39 - Bizim Proje Git);
+		System.out.println("18:39 - Bizim Proje Git");
 
 	}
 
